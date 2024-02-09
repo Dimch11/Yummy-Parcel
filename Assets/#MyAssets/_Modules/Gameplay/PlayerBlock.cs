dce6fd7cb8873ad46cfdc11a548efcdb61b50232
Dimch11 <87477131+Dimch11@users.noreply.github.com>
2024-02-09 17:15:52 +0400

    vfx

--- Assets/#MyAssets/_Modules/Gameplay/PlayerBlock.cs
+++ Assets/#MyAssets/_Modules/Gameplay/PlayerBlock.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UniRx;
@@ -6,6 +7,9 @@ using Zenject;
 
 public class PlayerBlock : MonoBehaviour
 {
+    [HideInInspector]
+    public bool canJump = true;
+
     Rigidbody2D rb;
 
     [Inject]
@@ -18,29 +22,37 @@ public class PlayerBlock : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
 
         arrowController.left
+            .Where(_ => canJump)
             .Subscribe(_ =>
             {
+                canJump = false;
                 rb.velocity = Vector2.left * gameplaySettings.speed;
             })
             .AddTo(this);
 
         arrowController.right
+            .Where(_ => canJump)
             .Subscribe(_ =>
             {
+                canJump = false;
                 rb.velocity = Vector2.right * gameplaySettings.speed;
             })
             .AddTo(this);
 
         arrowController.up
+            .Where(_ => canJump)
             .Subscribe(_ =>
             {
+                canJump = false;
                 rb.velocity = Vector2.up * gameplaySettings.speed;
             })
             .AddTo(this);
 
         arrowController.down
+            .Where(_ => canJump)
             .Subscribe(_ =>
             {
+                canJump = false;
                 rb.velocity = Vector2.down * gameplaySettings.speed;
             })
             .AddTo(this);
--- Assets/#MyAssets/_Modules/Gameplay/StaticBlock.cs
+++ Assets/#MyAssets/_Modules/Gameplay/StaticBlock.cs
@@ -4,5 +4,11 @@ using UnityEngine;
 
 public class StaticBlock : MonoBehaviour
 {
-    
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            collision.gameObject.GetComponent<PlayerBlock>().canJump = true;
+        }
+    }
 }
