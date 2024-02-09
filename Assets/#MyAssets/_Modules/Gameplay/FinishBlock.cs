0dc24f704a73c5ad05753754cf6a4ddd167efe45
Dimch11 <87477131+Dimch11@users.noreply.github.com>
2024-02-09 18:00:27 +0400

    save

--- Assets/#MyAssets/_Modules/Gameplay/FinishBlock.cs
+++ Assets/#MyAssets/_Modules/Gameplay/FinishBlock.cs
@@ -4,5 +4,12 @@ using UnityEngine;
 
 public class FinishBlock : MonoBehaviour
 {
-    
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            GetComponent<SpriteRenderer>().maskInteraction = SpriteMaskInteraction.VisibleInsideMask;
+            GetComponentInChildren<ParticleSystem>().Play();
+        }
+    }
 }
